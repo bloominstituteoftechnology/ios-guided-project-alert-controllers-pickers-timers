d7994e121888716fc06b8483b0606d3c57c5bb48
Paul Solt <PaulSolt@gmail.com>
2019-04-30 21:56:32 -0400

    Initial Commit

---
+++ CountdownDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountdownDemo
+//
+//  Created by Paul Solt on 4/30/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
